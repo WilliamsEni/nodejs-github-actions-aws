5fb19d45fa1b45613a74cc586c6b75ff1e265fc5
WilliamsEni <olawaleadedejieniola@gmail.com>
2026-07-19 10:17:07 +0000

    Update homepage for CI CD deployment demonstration

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const express = require('express');
+const express = require("express");
+
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>Node.js CI/CD Deployment on AWS</h1>
+    <p>This application was automatically deployed using GitHub Actions.</p>
+  `);
+});
 
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`App running on http://localhost:${port}`);
+});
